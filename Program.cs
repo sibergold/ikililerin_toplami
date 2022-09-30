908e5f682adfb204233d700fed6015fc78203636
sibergold <kaynakca21@hotmail.com>
2022-09-30 19:14:41 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ikililerin_toplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("Kaç sayı giriceksin? Sayı adedi çift olmalı.");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] ikililer=new int[n];
+            for(int j=0;j<n;j++)
+            {   Console.WriteLine("Lütfen {0}. sayıyı gir.",j+1);
+                ikililer[j]=Convert.ToInt32(Console.ReadLine());
+            }
+            
+            for(int i=0;i<n;i+=2)
+            {
+                if(ikililer[i]==ikililer[i+1])
+                {
+                    Console.Write(Math.Pow((ikililer[i]+ikililer[i+1]),2)+" ");
+                }   
+              else
+              {
+                Console.Write((ikililer[i]+ikililer[i+1])+" ");
+              }
+            }
+        }
+
+
+    }
+}
